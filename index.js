a75889d49e9eb0449bc09db80d9658073850adc8
Dheeraj <dheeraj.kumar@ucalgary.ca>
2020-11-11 11:43:44 -0700

    up to color

--- index.js
+++ index.js
@@ -1,34 +1,88 @@
+const { disconnect } = require("process");
 
-var app = require('express')();
-var bodyParser = require('body-parser');
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/styles.css", (req, res) => {
+  res.sendFile(__dirname + "/styles.css");
 });
 
-app.get('/styles.css', (req, res) => {
-    res.sendFile(__dirname + '/styles.css');
-});
+let totalPeople = 0;
+let usernames = [];
+io.on("connection", (socket) => {
+  totalPeople = totalPeople + 1;
+  let person = {
+    username: "User" + totalPeople,
+    color: "RRGGBB",
+  };
+  usernames.push(person.username);
+  socket.emit("welcome", "You are " + person.username);
+  io.emit("addingYou", {
+    username: person.username,
+    allusernames: usernames,
+  });
+  socket.on("chat message", (msg) => {
+    let datetime = new Date();
+    let dateString =
+      datetime.getMinutes() <= 9
+        ? datetime.getHours() + ":0" + datetime.getMinutes()
+        : datetime.getHours() + ":" + datetime.getMinutes();
+    if (msg[0] == "/") {
+      let changeMsg = msg.split(" ");
+      if (changeMsg.length == 2) {
+        if (changeMsg[0] == "/name") {
+          let newUsername = changeMsg[1];
+          if (!usernames.includes(newUsername)) {
+            for (let i = 0; i < usernames.length; i++) {
+              if (usernames[i] == person.username) {
+                usernames[i] = newUsername;
+              }
+            }
+            person.username = newUsername;
+            socket.emit("welcome", "You are " + person.username);
+            io.emit("addingYou", {
+              username: person.username,
+              allusernames: usernames,
+            });
+          } else {
+            console.log("this username is already being used");
+          }
+        } else if (changeMsg[0] == "/color") {
+          let newColor = changeMsg[1];
+          person.color = newColor;
+          socket.emit("colorChange", {
+            username: person.username,
+            color: person.color,
+          });
+        } else {
+          console.log("This is not a valid command");
+        }
+      } else {
+        console.log("This is not a valid command");
+      }
+    } else {
+      io.emit("chat message", dateString + " " + person.username + " " + msg);
+    }
+  });
+  socket.on("disconnect", () => {
+    for (let i = 0; i < usernames.length; i++) {
+      console.log(usernames[i]);
+      if (usernames[i] == person.username) {
+        usernames.splice(i, 1);
+      }
+    }
 
-// io.on('connection', (socket) => {
-//     console.log('a user connected');
-//     socket.on('disconnect', () => {
-//       console.log('user disconnected');
-//     });
-// });
-// io.on('connection', (socket) => {
-//     socket.on('chat message', (msg) => {
-//       console.log('message: ' + msg);
-//     });
-// });
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
+    io.emit("addingYou", {
+      username: person.username,
+      allusernames: usernames,
     });
+  });
 });
 
 http.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
